eda1574cd16486b126f265beeafce616f6c27b39
cooper <272553928@qq.com>
2024-04-29 21:02:55 +0800

    cmd order

--- cmd/cocoIM.go
+++ cmd/cocoIM.go
@@ -1,6 +1,10 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
 
 // 指定配置目录
 var ConfigPath string
@@ -8,15 +12,26 @@ var ConfigPath string
 func init() {
 	//cobra启动时候需要运行的方法
 	cobra.OnInitialize(initConfig)
-
+	// 设置提示选项
+	rootCmd.PersistentFlags().StringVar(&ConfigPath, "config", "./config.yaml", "config file (default is ./config.yaml)")
 }
 
-var rootCmd = cobra.Command{
+var rootCmd = &cobra.Command{
 	Use:   "coco",
 	Short: "一个很装杯的IM系统",
+	Run:   cocoIM,
 }
 
-func cocoIM() {}
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func cocoIM(cmd *cobra.Command, args []string) {
+
+}
 
 // 初始化配置
 func initConfig() {
